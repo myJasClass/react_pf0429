52e999d9802f9b608d26af38ef000291b213415b
phoebe70100 <phoebe70100@naver.com>
2022-04-29 22:24:12 +0900

    Link/NavLink

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+    const active = { color: 'aqua' }
     return (
         <header>
             <div className='inner'>
-                <h1><a href="#">LOGO</a></h1>
+                <h1>
+                    {/* <Link exact='true' to='/ */}
+                    <NavLink exact to='/' activeStyle={active}>LOGO</NavLink>
+                </h1>
                 <ul id="gnb">
-                    <li><a href="#">Gallery</a></li>
-                    <li><a href="#">Youtube</a></li>
+                    <li><NavLink to='/gallery' activeStyle={active}>Gallery</NavLink></li>
+                    <li><NavLink to='/youtube' activeStyle={active}>Youtube</NavLink></li>
                 </ul>
             </div>
         </header>
