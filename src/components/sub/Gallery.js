8a0a4042a6e82da01ab82d9a21d458f4912c08ea
phoebe70100 <phoebe70100@naver.com>
2022-04-29 23:47:18 +0900

    subLayout

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Gallery() {
     return (
-        <section>
-            <h1>Gallery</h1>
+        <section className='content gallery'>
+            <figure></figure>
+            <div className='inner'>
+                <h1>Gallery</h1>
+            </div>
         </section>
     )
 }
