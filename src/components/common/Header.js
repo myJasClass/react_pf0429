7708a4fea08d29d9354a5df55866d1c1b1e33c36
phoebe70100 <phoebe70100@naver.com>
2022-04-29 21:28:46 +0900

    header styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,8 +3,13 @@ import React from 'react'
 function Header() {
     return (
         <header>
-            <h1>LOGO</h1>
-
+            <div className='inner'>
+                <h1><a href="#">LOGO</a></h1>
+                <ul id="gnb">
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Youtube</a></li>
+                </ul>
+            </div>
         </header>
     )
 }
