f161bd28aafcbf3529f0b9224b1226cdda658c21
phoebe70100 <phoebe70100@naver.com>
2022-04-29 20:04:14 +0900

    main component 구성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './scss/style.scss';
+import Header from './components/common/Header';
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
     <>
-
+      <Header />
+      <Visual />
+      <Content />
+      <Footer />
     </>
   );
 }
