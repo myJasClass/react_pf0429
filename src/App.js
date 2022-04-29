6f062d859c1cceac4ab8da8fe8e606b8c1db15f9
phoebe70100 <phoebe70100@naver.com>
2022-04-29 20:15:14 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import { Route } from 'react-router-dom';
 import './scss/style.scss';
+
+//common
 import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+//main
 import Visual from './components/main/Visual';
 import Content from './components/main/Content';
-import Footer from './components/common/Footer';
+//sub
+import Youtube from './components/sub/Youtube';
+import Gallery from './components/sub/Gallery';
 
 function App() {
   return (
     <>
       <Header />
-      <Visual />
-      <Content />
+      <Route exact path='/'>
+        <Visual />
+        <Content />
+      </Route>
+
+      <Route path='/youtube' component={Youtube} />
+      <Route path='/gallery' component={Gallery} />
+
+      {/* <Route path='/gallery'>
+        <Gallery />
+      </Route> */}
+
       <Footer />
     </>
   );
